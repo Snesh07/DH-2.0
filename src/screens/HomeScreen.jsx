a9ad568e5f346a2e5ab1f35ebd42cc6b0f681804
Sneha Sharma <87299768+SynonymOfLove@users.noreply.github.com>
2024-01-11 14:42:43 +0530

    .

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Caroseulslider from "../components/Caroseulslider";
 import { Navbar } from "../components/Navbar";
-import { bannerimages, searchcardimages } from "../components/Images4Banner.js";
+import { bannerimages } from "../components/Images4Banner.js";
 import { WhyChoose } from "../components/WhyChoose";
 import ExProp from "../components/ExProp";
 
